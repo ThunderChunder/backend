fb5796420fa7a1920c5316b333fe58434a2660f8
Brian Valenzi <b_valenzi@hotmail.com>
2021-12-10 13:15:52 +1100

    added model validation on insurance form

--- backend/Controllers/InsuranceFormAPI/Models/InsuranceFormModel.cs
+++ backend/Controllers/InsuranceFormAPI/Models/InsuranceFormModel.cs
@@ -1,19 +1,28 @@
-﻿namespace Webservice.Controllers.InsuranceFormAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webservice.Controllers.InsuranceFormAPI.Models
 {
     public class InsuranceFormModel
     {
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
-
+        [Required]
+        [Range(18, 105)]
         public int Age { get; set; }
-
+        [Required]
         public string DOB { get; set; }
-
+        [Required]
         public string Occupation { get; set; }
-
+        [Required]
+        [Range(1000, 1000000)]
         public int SumInsured { get; set; }
-
+        [Required]
         public int MonthlyExpenses { get; set; }
-
+        [Required]
         public string State { get; set; }
+        [Required]
+        [RegularExpression(@"[0-9][0-9][0-9][0-9]")]
+        public string PostCode { get; set; }
     }
 }
